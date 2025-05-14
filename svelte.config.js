75172d3c4aa76334778bf573e03d0c6b7020b0ac
Egemen Gulpinar <egemengulpinar@gmail.com>
2025-05-14 15:17:52 +0200

    fixed "dotenv" warning problem

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,35 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { preprocessMeltUI } from '@melt-ui/pp';
+import sequence from 'svelte-sequential-preprocessor';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
+	preprocess: sequence([
 		vitePreprocess(),
 		preprocess({
 			postcss: true
-		})
-	],
+		}),
+		preprocessMeltUI()
+	]),
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
 			base: '/prolific-xai-904dm',
+		},
+		env: {
+			dir: '.',
+			publicPrefix: 'PUBLIC_',
+			privatePrefix: ''
 		}
-	},
+	}
 };
 
 export default config;
